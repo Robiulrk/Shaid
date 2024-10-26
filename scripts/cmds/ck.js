2ed6ed40461e3b74c435f27761cc8edebbe01db0
Robiulrk <shahidul7993@gmail.com>
2024-10-26 12:50:42 +0600

    Create ck.js

---
+++ scripts/cmds/ck.js
@@ -0,0 +1,22 @@
+ module.exports = {
+ config: {
+	 name: "ck",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "prefix",
+	 longDescription: "prefix",
+	 category: "prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "ck") {
+ return message.reply({
+ body: " 「 🥰❥︎----ღ᭄_রবিউল বসের পক্ষ থেকে সবাইকে I Love You❞࿐🥰🫠\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗥𝗢𝗕𝗜𝗨𝗟 𝗞𝗜𝗡𝗚」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/t15rB0W.mp4","https://i.imgur.com/qnXXBkE.mp4","https://i.imgur.com/qx6VpNR.mp4","https://i.imgur.com/wJWh62w.mp4")
+ });
+ }
+ }
+}

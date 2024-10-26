2952f4b9ad1fb93799e8565262a1863ae29f1c6b
Robiulrk <shahidul7993@gmail.com>
2024-10-26 13:35:50 +0600

    Update ck.js

--- scripts/cmds/ck.js
+++ scripts/cmds/ck.js
@@ -15,8 +15,14 @@
  if (event.body && event.body.toLowerCase() === "ck") {
  return message.reply({
  body: " 「 🥰❥︎----ღ᭄_রবিউল বসের পক্ষ থেকে সবাইকে I Love You❞࿐🥰🫠\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗥𝗢𝗕𝗜𝗨𝗟 𝗞𝗜𝗡𝗚」",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/t15rB0W.mp4","https://i.imgur.com/qnXXBkE.mp4","https://i.imgur.com/qx6VpNR.mp4","https://i.imgur.com/wJWh62w.mp4")
- });
+ attachment: await global.utils.getStreamFromURL
+	 var link=[
+	"https://i.imgur.com/t15rB0W.mp4",
+	 "https://i.imgur.com/qnXXBkE.mp4",
+	 "https://i.imgur.com/qx6VpNR.mp4",
+	 "https://i.imgur.com/wJWh62w.mp4"
+ ]
+	 });
  }
  }
 }

95547a1cdf63aeb5a12c4c84e875c7d1ec9e0a1f
Robiulrk <shahidul7993@gmail.com>
2024-10-26 07:26:10 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -15,13 +15,13 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 [.]\n
+𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 [/]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
 ➥ ✓help [number of page] -> see commands
 ➥ ✓sim [message] -> talk to bot
 ➥ ✓callad [message] -> report any problem encountered
-➥ ✓help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬\n𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/pinikjoy4200`,
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/82re01F.jpeg")
+➥ ✓help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 𝗥𝗢𝗕𝗜𝗨𝗟 𝗜𝗦𝗟𝗔𝗠\n𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞:https://www.facebook.com/profile.php?id=100093774930731&mibextid=kFxxJD `,
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/wJgbuvm.jpeg")
  });
  }
  }
